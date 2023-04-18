67ea345debbba7058d2ee931c9f951e4df80f087
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-18 12:12:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "web-1"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "websg-2"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-02eb7a4783e7e9317"
+}
+
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC."
   default     = "10.0.0.0/16"
