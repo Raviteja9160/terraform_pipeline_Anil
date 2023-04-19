513ddfaf6660be277317c4bf3617ff3d509b8fec
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-19 15:59:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block = var.vpc_cidr_block
 
   tags = {
     Name = "my-vpc"
-  }
-}
-
-# Create public subnets
-resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)
-
-  cidr_block = var.public_subnet_cidrs[count.index]
-  vpc_id     = aws_vpc.main.id
-
-  tags = {
-    Name = "public-subnet-${count.index + 1}"
-  }
-}
-
-# Create private subnets
-resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidrs)
-
-  cidr_block = var.private_subnet_cidrs[count.index]
-  vpc_id     = aws_vpc.main.id
-
-  tags = {
-    Name = "private-subnet-${count.index + 1}"
   }
 }
 
@@ -40,8 +20,32 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-# Create route table for public subnets
-resource "aws_route_table" "public" {
+# Create public subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  cidr_block = var.public_subnet_cidr_blocks[count.index]
+  vpc_id     = aws_vpc.main.id
+
+  tags = {
+    Name = "public-${count.index}"
+  }
+}
+
+# Create private subnets
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  cidr_block = var.private_subnet_cidr_blocks[count.index]
+  vpc_id     = aws_vpc.main.id
+
+  tags = {
+    Name = "private-${count.index}"
+  }
+}
+
+# Create route table
+resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -50,8 +54,34 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "public-route-table"
+    Name = "my-route-table"
   }
 }
 
+# Associate public subnets with route table
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr_blocks)
 
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create security group for EC2 instance
+resource "aws_security_group" "instance" {
+  name_prefix = "instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
