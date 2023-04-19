a5be87d20d33588e5dc8a0dc14061c3c7981022d
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-19 16:42:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,49 @@
 variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-west-2"
+  description = "The AWS region in which to create resources"
+  default = "us-west-2"
 }
 
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  description = "The CIDR block for the VPC"
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr_blocks" {
-  description = "CIDR blocks for the public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "vpc_name" {
+  description = "The name of the VPC"
+  default = "my-vpc"
 }
 
-variable "private_subnet_cidr_blocks" {
-  description = "CIDR blocks for the private subnets"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+variable "internet_gateway_name" {
+  description = "The name of the internet gateway"
+  default = "my-internet-gateway"
 }
 
-variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  type        = string
-  default     = "t2.micro"
+variable "public_subnet_cidr_block" {
+  description = "The CIDR block for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_availability_zone" {
+  description = "The availability zone for the public subnet"
+  default = "us-west-2a"
+}
+
+variable "public_subnet_name" {
+  description = "The name of the public subnet"
+  default = "my-public-subnet"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "The CIDR block for the private subnet"
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_availability_zone" {
+  description = "The availability zone for the private subnet"
+  default = "us-west-2b"
+}
+
+variable "private_subnet_name" {
+  description = "The name of the private subnet"
+  default = "my-private-subnet
 }
