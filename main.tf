e63bde49983c3202f0a412202b721682ecb2380e
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-20 10:22:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,55 @@
-# Define the provider and region
 provider "aws" {
   region = var.aws_region
 }
 
-# Create the VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = var.vpc_name
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
   }
 }
 
-# Create the internet gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = var.internet_gateway_name
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
   }
 }
 
-# Create the public subnet
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr_block
-  availability_zone = var.public_subnet_availability_zone
-  tags = {
-    Name = var.public_subnet_name
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
   }
-}
-
-# Create the private subnet
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr_block
-  availability_zone = var.private_subnet_availability_zone
-  tags = {
-    Name = var.private_subnet_name
-  }
-}
-
-# Create the route table
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = var.route_table_name
-  }
-}
-
-# Associate the public subnet with the route table
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
-  route_table_id = aws_route_table.main.id
-}
-
-# Associate the private subnet with the route table
-resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.private.id
-  route_table_id = aws_route_table.main.id
 }
